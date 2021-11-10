4a962fe06718af4c4d872dbecda57c84400291de
ckieverentineas <ckieverentineas@yandex.ru>
2021-11-10 16:02:35 +1000

    init project

---
+++ backend/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+app.use(cors())
+app.get('/user', async (req, res) => {
+  const posts = {
+      email: "amino@ami.no",
+      password: "2213sfs"
+  }
+  res.json(posts)
+})
+const server = app.listen(3000)
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{got}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      got: {}
+    }
+  },
+  async created() {
+  // GET request using fetch with async/await
+  const response = await fetch("http://localhost:3000/user");
+  const data = await response.json();
+  this.got = data;
+}
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
